663a1cec761bd82b6df381d8fed20a53b684cb96
Anna3001 <aniaw3001@icloud.com>
2023-11-17 18:04:26 +0100

    adding empty dinners views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :dinners
   resources :categories
   get 'recipies/new'
   get 'recipies/create'
---
+++ test/system/dinners_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+
+class DinnersTest < ApplicationSystemTestCase
+  setup do
+    @dinner = dinners(:one)
+  end
+
+  test "visiting the index" do
+    visit dinners_url
+    assert_selector "h1", text: "Dinners"
+  end
+
+  test "should create dinner" do
+    visit dinners_url
+    click_on "New dinner"
+
+    click_on "Create Dinner"
+
+    assert_text "Dinner was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Dinner" do
+    visit dinner_url(@dinner)
+    click_on "Edit this dinner", match: :first
+
+    click_on "Update Dinner"
+
+    assert_text "Dinner was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Dinner" do
+    visit dinner_url(@dinner)
+    click_on "Destroy this dinner", match: :first
+
+    assert_text "Dinner was successfully destroyed"
+  end
+end
